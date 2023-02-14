244cedab2a26eda4c5b22bc848d6291daba12c44
Shyam Bharati <sunny8r0@gmail.com>
2023-02-14 16:35:12 +0530

    initial commit

---
+++ myMathLibrary/src/main/java/com/example/mymathlibrary/MathOperations.java
@@ -0,0 +1,18 @@
+package com.example.mymathlibrary;
+
+import android.content.Context;
+
+public class MathOperations {
+    public static int addition(Context c,int a,int b) {
+        return a+b;
+    }
+    public static int multiply(Context c,int a,int b) {
+        return a*b;
+    }
+    public static int subtraction(Context c,int a,int b) {
+        return a-b;
+    }
+    public static int division(Context c,int a,int b) {
+        return a/b;
+    }
+}
