342960dd62b25191b3e24815961611c2b8ebe457
Shyam Bharati <sunny8r0@gmail.com>
2023-02-14 18:11:43 +0530

    final try

--- app/src/main/java/com/example/mathoperationlibrary/MainActivity.java
+++ app/src/main/java/com/example/mathoperationlibrary/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.mathoperationlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.mymathlibrary.MathOperations;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int sum = MathOperations.addition(this,12,12);
+
     }
 }
